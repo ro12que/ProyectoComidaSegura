67373ed53f9d9d093b2fe81ae977a2e0a1a8f896
lio <roque12morel@gmail.com>
2023-11-07 23:11:22 -0300

    Cambios al  JS

--- resultados/main.js
+++ resultados/main.js
@@ -4,26 +4,34 @@ var objeto = {
     foto: foto.jpg
 }
 
+function tagsDesignacion () {
+    var Celiaco = getElementsById("botonCeliaco");
+    /* agregar los demas vars */ 
+}
 
-var resultado = array.forEach(element => {
-        card.innerHTML()=`
-        <div class="col">
-            <div class="card h-100">
-              <img src= `;{objeto.foto}` class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">`;{objeto.nombre}`</h5>
-                <p class="card-text">`;{objeto.descripcion}`</p>
-              </div>
-              <div class="card-footer">
-                <!-- <small class="text-body-secondary">Last updated 3 mins ago</small> -->
-                <img class="sellos" src="" alt="durazno" />
-              </div>
-            </div>
-        </div>`
+function ejecutarBusqueda (tags, r, q) {
+    if (tags.Celiaco = true) {
+
+        /* manda la foto */
+
+    }/* agegar los demas tags */
+
+    sellosValor = 7;
+    var sellosAsignacion = document.getElementsByClassName("sellos").src = sellosValor;
+    var resultado = array.forEach(element => {
+            card.innerHTML()=`
+            <div class="col">
+                <div class="card h-100">
+                    <img src= `;{objeto.foto}` class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">`;{objeto.product_name}`</h5>
+                    <p class="card-text">`;{objeto.descripcion}`</p>
+                </div>
+                <div class="card-footer">
+                    <!-- <small class="text-body-secondary">Last updated 3 mins ago</small> -->
+                    <img class="sellos" src="" alt="durazno" />
+                </div>
+                </div>
+            </div>`
 });
-
-
-
-
-sellosValor = 7;
-var sellosAsignacion = document.getElementsByClassName("sellos").src = sellosValor
+}
