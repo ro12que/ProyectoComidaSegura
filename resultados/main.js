87c91bb547611e115140ae29e61b03d97e8c0159
lio <roque12morel@gmail.com>
2023-11-14 22:43:27 -0300

    js boceto

--- resultados/main.js
+++ resultados/main.js
@@ -1,37 +1,40 @@
-var objeto = {
-    titulo: galletita,
-    descripcion: descripcionlol,
-    foto: foto.jpg
-}
+const API = `https://world.openfoodfacts.net/api/v2/product` ;
 
-function tagsDesignacion () {
-    var Celiaco = getElementsById("botonCeliaco");
-    /* agregar los demas vars */ 
-}
+const userSearch = getElementsById("search".valueOf());
 
-function ejecutarBusqueda (tags, r, q) {
-    if (tags.Celiaco = true) {
-
-        /* manda la foto */
-
-    }/* agegar los demas tags */
-
-    sellosValor = 7;
-    var sellosAsignacion = document.getElementsByClassName("sellos").src = sellosValor;
-    var resultado = array.forEach(element => {
-            card.innerHTML()=`
+const respuesta = fetch (API/userSearch)
+    .then((Response)=> Response.json())
+    .then((userSearch)=>{
+    const resp = data.map((userSearch)=>{        
+    card.innerHTML()=`
             <div class="col">
                 <div class="card h-100">
-                    <img src= `;{objeto.foto}` class="card-img-top" alt="...">
+                    <img src= `;{Response.foto}` class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">`;{objeto.product_name}`</h5>
+                    <h5 class="card-title">`;{Response.product_name}`</h5>
                     <p class="card-text">`;{objeto.descripcion}`</p>
                 </div>
                 <div class="card-footer">
                     <!-- <small class="text-body-secondary">Last updated 3 mins ago</small> -->
-                    <img class="sellos" src="" alt="durazno" />
+                    <img class="sellos" src=`;{sellosValor}` alt="durazno" />
                 </div>
                 </div>
             </div>`
+            });
+        },
+)
+
+function tagsDesignacion () {
+    var Celiaco = getElementsById("botonCeliaco");
+    var Diabetico = getElementsById("botonDiabetico");
+    var Vegano = getElementsById("botonVegano");
+    var Vegetariano = getElementsById("botonVegetariano");
+    var Hipertenso = getElementsById("botonHipertenso");
+    var IntLactosa = getElementsById("botonIntLactosa");
+};
+
+    sellosValor = 7;
+    var sellosAsignacion = document.getElementsByClassName("sellos").src = sellosValor;
+    var resultado = array.forEach(element => {
+
 });
-}
