07737d3fc1ddc02e48dcbd13c22eec8c31dfdc2c
lio <roque12morel@gmail.com>
2023-12-13 20:06:38 -0300

    arreglo filtros

--- resultados/main.js
+++ resultados/main.js
@@ -1,6 +1,4 @@
-const API = `https://world.openfoodfacts.net/api/v2/search`;
-
-//------------------SISTEMA DE BOTONES-----------------------------------
+//------------------SISTEMA CAPTURA DE BOTONES-----------------------------------
 
 var Celiaco = document.getElementById("botonCeliaco");//LISTO
 var Diabetico = document.getElementById("botonDiabetico");//LISTO
@@ -9,148 +7,157 @@ var Vegetariano = document.getElementById("botonVegetariano");//LISTO
 var Hipertenso = document.getElementById("botonHipertenso");//LISTO
 var IntoLactosa = document.getElementById("botonIntoLactosa");//LISTO 6
 
-var TagDesignador = []
+var TagDesignador = [] //guarda los valores de los  botones que se clickean
 
-Celiaco.addEventListener('click', ()=>{
-if(TagDesignador.includes('Celiaco') === false){
-    Celiaco = true;
-    console.log(Celiaco)
-    TagDesignador.push('Celiaco')
-    console.log(TagDesignador)
-    }else{
-        TagDesignador = TagDesignador.filter(tag=> tag != 'Celiaco');
+Celiaco.addEventListener('click', () => {
+    if (TagDesignador.includes('Celiaco') === false) {
+        Celiaco = true;
+        console.log(Celiaco)
+        TagDesignador.push('Celiaco')
         console.log(TagDesignador)
-    }
-});
-            
-Diabetico.addEventListener('click', ()=>{
-if(TagDesignador.includes('Diabetico') === false){
-    Diabetico = true;
-    console.log(Diabetico)
-    TagDesignador.push('Diabetico')
-    console.log(TagDesignador)
-    }else{
-        TagDesignador = TagDesignador.filter(tag=> tag != 'Diabetico');
+    } else {
+        TagDesignador = TagDesignador.filter(tag => tag != 'Celiaco');
         console.log(TagDesignador)
     }
 });
 
-Vegano.addEventListener('click', ()=>{
-if(TagDesignador.includes('Vegano') === false){
-    Vegano = true;
-    console.log(Vegano)
-    TagDesignador.push('Vegano')
-    console.log(TagDesignador)
-    }else{
-        TagDesignador = TagDesignador.filter(tag=> tag != 'Vegano');
+Diabetico.addEventListener('click', () => {
+    if (TagDesignador.includes('Diabetico') === false) {
+        Diabetico = true;
+        console.log(Diabetico)
+        TagDesignador.push('Diabetico')
+        console.log(TagDesignador)
+    } else {
+        TagDesignador = TagDesignador.filter(tag => tag != 'Diabetico');
         console.log(TagDesignador)
     }
 });
 
-Vegetariano.addEventListener('click', ()=>{
-if(TagDesignador.includes('Vegetariano') === false){
-    Vegetariano = true;
-    console.log(Vegetariano)
-    TagDesignador.push('Vegetariano')
-    console.log(TagDesignador)
-    }else{
-        TagDesignador = TagDesignador.filter(tag=> tag != 'Vegetariano');
+Vegano.addEventListener('click', () => {
+    if (TagDesignador.includes('Vegano') === false) {
+        Vegano = true;
+        console.log(Vegano)
+        TagDesignador.push('Vegano')
+        console.log(TagDesignador)
+    } else {
+        TagDesignador = TagDesignador.filter(tag => tag != 'Vegano');
         console.log(TagDesignador)
     }
 });
 
-Hipertenso.addEventListener('click', ()=>{
-if(TagDesignador.includes('Hipertenso') === false){
-    Hipertenso = true;
-    console.log(Hipertenso)
-    TagDesignador.push('Hipertenso')
-    console.log(TagDesignador)
-    }else{
-        TagDesignador = TagDesignador.filter(tag=> tag != 'Hipertenso');
+Vegetariano.addEventListener('click', () => {
+    if (TagDesignador.includes('Vegetariano') === false) {
+        Vegetariano = true;
+        console.log(Vegetariano)
+        TagDesignador.push('Vegetariano')
+        console.log(TagDesignador)
+    } else {
+        TagDesignador = TagDesignador.filter(tag => tag != 'Vegetariano');
         console.log(TagDesignador)
     }
 });
 
-IntoLactosa.addEventListener('click', ()=>{
-if(TagDesignador.includes('intoLactosa') === false){
-    IntoLactosa = true;
-    console.log(IntoLactosa)
-    TagDesignador.push('intoLactosa')
-    console.log(TagDesignador)
-    }else{
-        TagDesignador = TagDesignador.filter(tag=> tag != 'intoLactosa');
+Hipertenso.addEventListener('click', () => {
+    if (TagDesignador.includes('Hipertenso') === false) {
+        Hipertenso = true;
+        console.log(Hipertenso)
+        TagDesignador.push('Hipertenso')
+        console.log(TagDesignador)
+    } else {
+        TagDesignador = TagDesignador.filter(tag => tag != 'Hipertenso');
+        console.log(TagDesignador)
+    }
+});
+
+IntoLactosa.addEventListener('click', () => {
+    if (TagDesignador.includes('intoLactosa') === false) {
+        IntoLactosa = true;
+        console.log(IntoLactosa)
+        TagDesignador.push('intoLactosa')
+        console.log(TagDesignador)
+    } else {
+        TagDesignador = TagDesignador.filter(tag => tag != 'intoLactosa');
         console.log(TagDesignador)
     }
 });
 
 
-//-------------------SISTEMA DE FETCH------------------------------------
+//---------------------SISTEMA DE FILTRADO------------------------
 
 const botonBusqueda = document.getElementById("botonBusqueda");
 
-botonBusqueda.addEventListener('click', function() {
+botonBusqueda.addEventListener('click', function () {
 
-    const userinput = document.getElementById('search')
+    const userinput = document.getElementById('search') //se toma la busqueda del usuario
     userSearch = userinput.value;
     console.log(userSearch);
 
-    const API = `https://es.openfoodfacts.org/cgi/search/pl`;
-    //https://es.openfoodfacts.org/cgi/search.pl?search_terms=${encodeURIComponent(userSearch)}
-
+    const API = `https://ar.openfoodfacts.org/cgi/search.pl`; //nuestra hermosa api
 
     const queryParams = {
-        search_terms: `${userSearch}`, // Reemplaza 'tu_termino_de_búsqueda' por tu término de búsqueda real
+        json:{},
+        search_terms: `${userSearch}`,
         page_size: 20,
-        countries_tags_en: "Argentina",
-        
-/*
-        allergens_from_ingredients: [],
-        nutrient_levels: {},
-        ingredients_analysis_tags: [],*/
+        action: 'process',
     };
 
     for (let i = 0; i < 6; i++) {
-    
+
         switch (TagDesignador[i]) {
-            case 'Diabetico':
-                queryParams.json.nutrient_levels.push('sugar: low');
+                case 'Diabetico':
+                queryParams.push(`tagtype_1: 'nutrient_levels',`);
+                queryParams.push(`tag_contains_1: 'contains',`)
+                queryParams.push(`tag_1: en: "sugar in low quantity"',`)
                 console.log('diabtico');
                 break;
-            case 'Hipertenso':
-                queryParams.json.nutrient_levels.push('salt: low');
+                case 'Hipertenso':
+                    queryParams.tagtype_2=`'nutrient_levels'`;
+                    queryParams.tag_contains_2=`'contains'`;
+                    queryParams.tag_2=`'en salt in low quantity',`;
                 console.log('hipertenso');
+                break; 
+                case 'Celiaco':
+                        queryParams.tagtype_3='allergens';
+                        queryParams.tag_contains_3='does_not_contain';
+                        queryParams.tag_3=`en:gluten`;
+                console.log('celiaco');
                 break;
-            case 'Celiaco':
-                queryParams.json.allergens_from_ingredients.push('gluten');
-console.log('celiaco');
+                case 'IntoLactosa':
+                    queryParams.tagtype_4= `'allergens'`;
+                    queryParams.tag_contains_4=`'does_not_contain'`;
+                    queryParams.tag_4= `'en: milk'`;
+                console.log('intolactosa');
+                break; 
+                case 'Vegetariano':
+                    queryParams.push(`tagtype_5: 'ingredients-analysis',`);
+                    queryParams.push(`tag_contains_5: 'contains',`)
+                    queryParams.push(`tag_5: 'en:vegetarian',`)
+                console.log('vegetariano');
                 break;
-            case 'IntoLactosa':
-                queryParams.json.allergens_from_ingredients.push('lactose');
-console.log('intolactosa');
-                break;
-            case 'Vegetariano':
-                queryParams.json.ingredients_analysis_tags.push('en:vegetarian');
-console.log('vegetariano');
-                break;
-            case 'Vegano':
-                queryParams.json.ingredients_analysis_tags.push('en:vegan');
-console.log('vegano');
+                case 'Vegano':
+                    queryParams.push(`tagtype_6: 'ingredients-analysis',`);
+                    queryParams.push(`tag_contains_6: 'contains',`)
+                    queryParams.push(`tag_6: 'en: vegan',`)
+                console.log('vegano');
                 break;
             default:
-    
+
                 break;
         }
     };
-    
+
     const queryString = new URLSearchParams(queryParams).toString();
     console.log(queryString)
     const APIProductSearch = `${API}?${queryString}`;
     console.log(APIProductSearch);
     console.log(queryParams);
 
-    //const APIProductSearch = `https://es.openfoodfacts.org/cgi/search.pl?search_terms=${encodeURIComponent(userSearch)}&page_size=20&json=true`;    
+
+//-------------------SISTEMA DE FETCH------------------------------------
+
+
     const createRes = document.getElementById('ResultadoCointainer');
-    const respuesta = fetch (APIProductSearch)
+    const respuesta = fetch(APIProductSearch)
         .then(res => res.json())
         .then(data => {
             console.log(data)
@@ -172,13 +179,13 @@ console.log('vegano');
                                 </div> 
                         </div> 
                     </div>`;
-                                    });
-            })
-            .catch(error => {
-                console.error('error fecht-data', error);
-               let  carderror=document.createElement("div");
-               createRes.appendChild(carderror);
-               carderror.innerHTML= `
+            });
+        })
+        .catch(error => {
+            console.error('error fecht-data', error);
+            let carderror = document.createElement("div");
+            createRes.appendChild(carderror);
+            carderror.innerHTML = `
                <div class="errorcard"> 
                   <div class="errorimg1"> 
                        <img src=  class="errorimg" alt="errorfoto">
@@ -194,17 +201,7 @@ console.log('vegano');
            
                     </div>
            </div>`;
-               
-
-            })
+        })
 
 })
 
-/*                                    <!--<img class="sellos" src=${sellosValor} alt="durazno"/>
-                                    <img class="sellos" src=${sellosValordos} alt="durazno"/> 
-                                    <img class="sellos" src=${sellosValortres} alt="durazno"/> -->
-    sellosValor = 7;
-    var sellosAsignacion = document.getElementsByClassName("sellos").src = sellosValor;
-    var resultado = array.forEach(element => {
-
-});*/
